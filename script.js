67a56de873e4d3883bef08c0a8010cac1384c132
Aqeem Abdullah <77499306+aqeemabd@users.noreply.github.com>
2021-01-22 07:45:30 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const name = document.getElementById('name')
+const password = document.getElementById('password')
+const form = document.getElementById('form')
+const errorElement = document.getElementById('error')
+
+form.addEventListener('submit', (e) =>{
+    let messages = []
+    if(name.value === '' || name.value == null){
+        messages.push('Name is required')
+    }
+
+    if(password.value.length <= 6){
+        messages.push('Password must be longer than 6 characters')
+    }
+
+    if(password.value.length >= 15){
+        messages.push('Password must be less than 15 characters')
+    }
+
+    if(password.value === 'password'){
+        messages.push('Password cannot be "password"')
+    }
+
+    if(messages.length > 0){
+        e.preventDefault()
+        errorElement.innerText = messages.join(', ') 
+    }
+})
+
